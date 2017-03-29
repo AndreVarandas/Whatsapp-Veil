6ed2f927a4bf7f7515258557a1bf3c3fde386ff1
Andre Varandas <andre.varandas@tekever.com>
2017-03-29 10:25:20 +0100

    (feat) - Replaced black veil with cat images.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Safe-Whatsapp
-Make WhatsApp Safe for work.
+# Whatsapp Veil
+Make WhatsApp Safe for work, by adding a "veil" to any image, gif or video
+thumbnail.
+
+You can choose adding either a black background or a cat image to replace
+the image thumbnail.
+
+[License - MIT](LICENSE)
--- whatsappveil.js
+++ whatsappveil.js
@@ -1,7 +1,26 @@
 'use strict';
-
 const debugMode = false;
-console.info('Up and running mf!');
+
+/**
+ * Array containing royal free flickr cat images
+ * @type {Array}
+ */
+const cats = [
+    "//c1.staticflickr.com/4/3894/14818650368_9147bee007_c.jpg",
+    "//c1.staticflickr.com/9/8215/8333619957_e3b5aff6f0_z.jpg",
+    "//c1.staticflickr.com/9/8065/8168159947_94b3a89a2a_z.jpg",
+    "//c1.staticflickr.com/6/5512/14410052054_6296fd600f.jpg",
+    "//c1.staticflickr.com/8/7073/7190755946_ea97e85765_z.jpg",
+    "//c1.staticflickr.com/4/3946/14934292534_32b0302f2d.jpg",
+    "//c1.staticflickr.com/6/5211/5513402618_3ce232e01a_z.jpg",
+    "//c1.staticflickr.com/8/7496/16236770082_205f4e358f_z.jpg",
+    "//c1.staticflickr.com/6/5141/5616147572_197d15f94d_z.jpg",
+    "//c1.staticflickr.com/7/6092/6330704947_dd7e1b453c.jpg"
+];
+
+const getRandomCatImage = () => {
+    return cats[Math.floor(Math.random() * (10 - 0) + 1)];
+};
 
 /**
  * Remove image opacity
@@ -10,12 +29,19 @@ console.info('Up and running mf!');
 const clearImages = () => {
   let images = document.querySelectorAll('div.image-thumb') || 0;
   for (let i = 0; i < images.length; i++) {
+    let image = images[i];
+    if (image.classList.contains('cat'))
+        continue;
+
+    image.classList.add('cat');
     let coverElementHolder = document.createElement('div');
     coverElementHolder.className = 'cover-element--holder';
+    coverElementHolder.style.backgroundImage = `url(${getRandomCatImage()})`;
+
     let coverElementText = document.createElement('span');
-    coverElementText.textContent = "NSFW";
+    coverElementText.textContent = "Click to show";
     coverElementText.className = 'cover-element--text';
-    let image = images[i];
+
     coverElementHolder.appendChild(coverElementText)
     image.appendChild(coverElementHolder);
   }
